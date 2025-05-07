cca2c4a7a0c235006b2021503f1b4000f477cab6
Vasu Garg <vgvasu35@gmail.com>
2025-05-07 15:14:53 +0530

    config

---
+++ _includes/config.inc.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * /_includes/config.SAMPLE.inc.php
+ *
+ * This file is part of DomainMOD, an open source domain and internet asset manager.
+ * Copyright (c) 2010-2025 Greg Chetcuti <greg@greg.ca>
+ *
+ * Project: http://domainmod.org   Author: https://greg.ca
+ *
+ * DomainMOD is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * DomainMOD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with DomainMOD. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ */
+?>
+<?php
+// Path Settings
+$web_root = '/dm'; // don't include trailing slash (if you're installing in the root just leave this blank)
+
+// Database Settings
+$dbhostname = 'n0w0c8s0g88s8gk08kwo0oss';
+$dbname = 'default';
+$dbusername = 'mysql';
+$dbpassword = 'QXtZUrOC72wkE7slgJ4p7sO2XTzw67saKa7hhQYOMPvummxYcvYRDP4EumQMPUcZ';
